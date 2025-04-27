ebcf08406ce846447a08c0ce234f0f47041988aa
Steven <steven52880@163.com>
2025-04-27 19:39:36 +0800

    ugetpid

--- kernel/proc.c
+++ kernel/proc.c
@@ -127,6 +127,15 @@ found:
     return 0;
   }
 
+  // An USYSCALL page
+  if ((p->syscallframe = (struct usyscall *)kalloc()) == 0)
+  {
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+  p->syscallframe->pid = p->pid;
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -153,7 +162,10 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
-  if(p->pagetable)
+  if (p->syscallframe)
+    kfree((void *)p->syscallframe);
+  p->syscallframe = 0;
+  if (p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
   p->sz = 0;
@@ -196,6 +208,15 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  // map the USYSCALL just below trapframe
+  if (mappages(pagetable, USYSCALL, PGSIZE,
+               (uint64)(p->syscallframe), PTE_R | PTE_U) < 0)
+  {
+    uvmunmap(pagetable, USYSCALL, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+
   return pagetable;
 }
 
@@ -206,6 +227,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
@@ -238,6 +260,9 @@ userinit(void)
   // prepare for the very first "return" from kernel to user.
   p->trapframe->epc = 0;      // user program counter
   p->trapframe->sp = PGSIZE;  // user stack pointer
+
+  // fill the initial syscall shared memory
+  p->syscallframe->pid = p->pid;
 
   safestrcpy(p->name, "initcode", sizeof(p->name));
   p->cwd = namei("/");
